84a04631dcba48601e6991790a4948f06fe3b104
LyonYang <LyonYang@users.noreply.github.com>
2017-11-17 16:33:15 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git config user.name "Lyon"
+git config user.email "547903993@qq.com"
+git checkout -b gitbook
+git status
+git add .
+git commit -m "[Travis] Update SUMMARY.md"
+git push -f "https://${GH_TOKEN}@${GH_REF}" gitbook:gitbook
+gitbook install
+gitbook build .
+if [ $? -ne 0 ];then
+    exit 1
+fi
+cd _book
+git init
+git checkout --orphan gh-pages
+git status
+sleep 5
+git add .
+git commit -m "Update gh-pages"
+git remote add origin git@github.com:LyonYang/blogs.git
+git push -f "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages
