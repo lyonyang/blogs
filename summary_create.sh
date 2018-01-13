2b64674bc765cea9026ec8a367f5ab5779af0f5d
lyonyang <547903993@qq.com>
2018-01-13 19:41:11 +0800

    modified: summary_create.sh

--- summary_create.sh
+++ summary_create.sh
@@ -7,7 +7,7 @@
 find `ls|egrep -v "_book|_other|node_modules"` -type f -name "*.md"|sed 's#README.md#00README.md#g'|sort|awk -F "/" '{if($NF!="00README.md") print $0"/" ;else print $0}' OFS="/"|sed  's#[^/]##g'|awk '{a=(length-1);while(a>0){printf "  ";a--}print "* "}' > /tmp/summary_1
 find `ls|egrep -v "_book|_other|node_modules"` -type f -name "*.md"|sed "s#README.md#00README.md#g"|sort|awk -F "[./]" '{if($(NF-1) != "00README") print $(NF-1)"]("$0")" ;else print $(NF-2)"]("$0")"}' > /tmp/summary_2
 paste -d "[" /tmp/summary_1 /tmp/summary_2 > tmp_SUMMARY.md
-sed 's#00README.md#README.md#g' tmp_SUMMARY.md |awk  '{if(NR==1)print "# Sumarry\n\n* [Introduction](README.md)";else print $0}' > SUMMARY.md && mv tmp_SUMMARY.md /tmp
+sed 's#00README.md#README.md#g' tmp_SUMMARY.md |awk  '{if(NR==1)print "# Sumarry\n\n* [lyonyang](README.md)";else print $0}' > SUMMARY.md && mv tmp_SUMMARY.md /tmp
 # 由于Mac下,sed -i参数必须要指定备份文件(虽然可以使用 -i "" 传递一个空字符,不备份,但是这种写法在Linux上会报错),所以这里不使用-i参数
 # 说明 -i ""在Mac下可以使用, -i"" 在Linux下可以使用,反过来都不可以使用
 #|sed 's#[0-9][0-9]-##g' 不能去掉数字,除非gitbook生成静态文件的时候,md文件也没有数字
